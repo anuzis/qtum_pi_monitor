a0028bde9cad4d562d343677f5bbdf81ae0db53b
ryzen <anuzis@gmail.com>
2017-10-12 15:06:46 -0400

    Initial qtum monitoring script for raspberry pi

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# qtum_pi_monitor
-Monitors QTUM nodes on a Raspberry Pi. Notifies of block wins, monitors temperature, and performs a few basic checks.
+# QTUM Raspberry Pi Monitor
+
+- Simple script to notify of block wins, monitor temperature, and performs basic sanity checks.
+- Under 100 lines (on release), easy to review and modify.
+
+# Setup Instructions
+- Set RECIPIENT_EMAIL to your address.
+- Set QTUM_PATH to where you have QTUM installed.
+- Add a cronjob to run the script at your desired frequency. (examples in script)
+
+Easy areas for improvement:
+- Report on weekly/monthly/overall growth relative to initial balance
+- Report on whether block win timing is lucky/unlucky relative to statistical expectation
---
+++ qtum_monitor.py
@@ -0,0 +1,99 @@
+""" Simple script to monitor QTUM wallet and notify when blocks are won.
+
+Runs via cron. Ref: https://www.howtoforge.com/a-short-introduction-to-cron-jobs
+Example running hourly. Change the first 0 to * if you want updates each minute.
+0 * * * * /usr/bin/python /home/pi/qtum_monitor.py
+
+First time the script runs it saves the current balance and stake data to a file.
+On subsequent runs, if 'stake' increases the script emails you at RECIPIENT_EMAIL
+"""
+import subprocess
+import json
+import os
+import sys
+import time
+
+# Configuration options
+NOTIFY_WINS_ONLY = True # notify on wins only, if False also reports general balance updates
+MONITOR_TEMPERATURE = True # only set true for Raspberry Pi or a system with similar temperature monitoring
+TEMPERATURE_WARNING_THRESHOLD = 80.0 # warn if temperature exceeds this threshold in Celcius
+
+# Assumes system is configured to use /usr/bin/mail.
+# Easy setup ref: http://www.raspberry-projects.com/pi/software_utilities/email/ssmtp-to-send-emails
+RECIPIENT_EMAIL = 'user@gmail.com'
+
+QTUM_PATH = '/home/pi/qtum/'
+LOG_FILE = QTUM_PATH + 'qtum_monitor.log'
+
+STATE_DATA = {
+    'initial_balance': 0.0,
+    'balance': 0.0,
+    'stake': 0.0,
+    'total_balance': 0.0,
+    'last_block_time_won': 0, # epoch seconds of last block win
+}
+
+if __name__ == '__main__':
+    # Get latest wallet info.
+    try:
+        wallet_info = json.loads(subprocess.check_output([QTUM_PATH + 'bin/qtum-cli', 'getwalletinfo']))
+        staking_info = json.loads(subprocess.check_output([QTUM_PATH + 'bin/qtum-cli', 'getstakinginfo']))
+    except subprocess.CalledProcessError:
+        cmd = 'echo "Error running qtum-cli. Verify settings" | /usr/bin/mail -s "Error running qtum-cli" %s' % RECIPIENT_EMAIL
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sys.exit()
+
+    # Pre-checks
+    if (not wallet_info['balance']) and (not wallet_info['stake']):
+        cmd = 'echo "No QTUM balance." | /usr/bin/mail -s "No QTUM balance" %s' % RECIPIENT_EMAIL
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sys.exit()
+    if staking_info['errors']:
+        cmd = 'echo "QTUM Errors: %s" | /usr/bin/mail -s "QTUM Errors" %s' % (str(staking_info['errors']), RECIPIENT_EMAIL)
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sys.exit()
+    if staking_info['enabled'] != True:
+        cmd = 'echo "QTUM Staking disabled." | /usr/bin/mail -s "QTUM staking disabled." %s' % RECIPIENT_EMAIL
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sys.exit()
+    if MONITOR_TEMPERATURE:
+        temp_str = subprocess.check_output(['/opt/vc/bin/vcgencmd', 'measure_temp'])
+        temp = float(temp_str[temp_str.find('=')+1:temp_str.find("'")])
+        if temp > TEMPERATURE_WARNING_THRESHOLD:
+            cmd = 'echo "QTUM Pi Temperature Warning! %fC above %fC" | /usr/bin/mail -s "QTUM Pi Temperature Warning" %s' % (temp, TEMPERATURE_WARNING_THRESHOLD, RECIPIENT_EMAIL)
+            ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    # Prepare relevant state data
+    latest_data = STATE_DATA.copy()
+    latest_data['balance'] = wallet_info['balance']
+    latest_data['stake'] = wallet_info['stake']
+    latest_data['total_balance'] = wallet_info['balance'] + wallet_info['stake']
+
+    # Read prior status for comparison, creating log file if none.
+    if not os.path.exists(LOG_FILE):
+        latest_data['initial_balance'] = wallet_info['balance']
+        f = open(LOG_FILE, 'w')
+        f.write(json.dumps(latest_data))
+        f.close()
+        cmd = 'echo "QTUM Monitor initialized" | /usr/sbin/ssmtp %s' % RECIPIENT_EMAIL
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        sys.exit()
+
+    # Read prior data.
+    log_file = open(LOG_FILE, 'r')
+    prior_data = json.loads(log_file.read())
+    log_file.close()
+
+    # Report on results
+    if latest_data['stake'] > prior_data['stake']:
+        latest_data['latest_block_time_won'] = int(time.time())
+        cmd = 'echo "Stake earned! Balance: %d Stake: %d" | /usr/bin/mail -s "Stake earned!" %s' % (int(latest_data['balance']), int(latest_data['stake']), RECIPIENT_EMAIL)
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if not NOTIFY_WINS_ONLY:
+        cmd = 'echo "Balance: %d Stake: %d" | /usr/bin/mail -s "Update" %s' % (int(latest_data['balance']), int(latest_data['stake']), RECIPIENT_EMAIL)
+        ssmpt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+    # Write latest state to log
+    log_file = open(LOG_FILE, 'w')
+    log_file.write(json.dumps(latest_data))
+    log_file.close()
